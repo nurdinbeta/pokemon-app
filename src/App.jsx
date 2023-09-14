d5be0d00319dd2727cf61e6d0357879eb73ec14d
Nurdin Beta <nurdinbeta@gmail.com>
2023-09-14 19:32:47 +0800

    feat: create homepage
    - create search bar
    - get pokemon using api
    - display pokemon name, image, and details
    - add error display when pokemon couldn't found
    
    will add some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonData, setPokemonData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchPokemonData = async () => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
+      setPokemonData(response.data);
+      setError(null);
+    } catch (err) {
+      setError('Pokemon not found. Please enter a valid name.');
+      setPokemonData(null);
+    }
+  };
 
   return (
-    <>
+    <div>
+      <h1>Pokemon App Finder</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input
+          type="text"
+          placeholder="Enter Pokemon Name"
+          value={pokemonName}
+          onChange={(e) => setPokemonName(e.target.value)}
+        />
+        <button
+          onClick={fetchPokemonData}
+        >
+          Search
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {error && <p>{error}</p>}
+      {pokemonData && (
+        <div>
+          <h2>{pokemonData.name}</h2>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`}
+            alt={pokemonData.name}
+          />
+          <h3>Pokemon Details:</h3>
+          <ul>
+            <li>Height: {pokemonData.height / 10} meters</li>
+            <li>Weight: {pokemonData.weight / 10} kilograms</li>
+            <li>Type(s): {pokemonData.types.map((type) => type.type.name).join(', ')}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
