3cd609019e6863630649eb93a0f82a759ace899e
Nurdin Beta <nurdinbeta@gmail.com>
2023-09-14 21:37:22 +0800

    style(homepage): add style
    - change background colour
    - add card
    - resize image

--- src/App.jsx
+++ src/App.jsx
@@ -17,36 +17,39 @@ function App() {
     }
   };
 
-  return (
-    <div>
-      <h1>Pokemon App Finder</h1>
-      <div>
+   return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-[#526D82] text-center">
+      <h1 className="text-5xl font-bold p-3 mb-4">Pokemon App Finder</h1>
+      <div className="mb-6">
         <input
           type="text"
+          className="px-4 py-2 border rounded-lg w-64"
           placeholder="Enter Pokemon Name"
           value={pokemonName}
           onChange={(e) => setPokemonName(e.target.value)}
         />
         <button
+          className="ml-3 px-6 py-2 bg-[#DDE6ED] text-black rounded-lg "
           onClick={fetchPokemonData}
         >
           Search
         </button>
       </div>
-      {error && <p>{error}</p>}
+      {error && <p className="text-red-500 p-40 text-xl font-bold mb-4">{error}</p>}
       {pokemonData && (
-        <div>
-          <h2>{pokemonData.name}</h2>
+        <div className='bg-[#9DB2BF] border-2 border-black shadow-black shadow-lg rounded-lg items-center w-1/4 h-3/4 mb-4'>
+          <h2 className="text-3xl p-4 font-semibold">{pokemonData.name.toUpperCase()}</h2>
           <img
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`}
             alt={pokemonData.name}
+            className="w-[360px] h-[360px] mx-auto mt-2"
           />
-          <h3>Pokemon Details:</h3>
-          <ul>
-            <li>Height: {pokemonData.height / 10} meters</li>
-            <li>Weight: {pokemonData.weight / 10} kilograms</li>
-            <li>Type(s): {pokemonData.types.map((type) => type.type.name).join(', ')}</li>
-          </ul>
+          <h3 className="text-xl text-center font-bold">Pokemon Details:</h3>
+          <h4 className="text-center text-lg px-4 py-1 font-medium">
+            <p>Height: {pokemonData.height / 10} meters</p>
+            <p>Weight: {pokemonData.weight / 10} kilograms</p>
+            <p>Type(s): {pokemonData.types.map((type) => type.type.name).join(', ')}</p>
+          </h4>
         </div>
       )}
     </div>
